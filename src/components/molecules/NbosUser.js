b4aceaab342cb7d4ec7db874a0f123784c4f8daf
spietrek <spietrek@gmail.com>
2021-11-15 19:39:22 -0500

    cleanup code

--- src/components/molecules/NbosUser.js
+++ src/components/molecules/NbosUser.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 export const NbosUser = ({ user }) => {
   let fullName = 'N/A';
@@ -21,24 +20,4 @@ export const NbosUser = ({ user }) => {
       <div>Team Lead Full Name: {teamLeadFullName}</div>
     </>
   );
-};
-
-NbosUser.propTypes = {
-  id: PropTypes.node,
-  teamLeadFirstName: PropTypes.string,
-  teamLeadLastName: PropTypes.string,
-  firstName: PropTypes.string,
-  lastName: PropTypes.string,
-  email: PropTypes.string,
-  role: PropTypes.string,
-};
-
-NbosUser.defaultProps = {
-  id: null,
-  teamLeadFirstName: null,
-  teamLeadLastName: null,
-  firstName: null,
-  lastName: null,
-  email: 'N/A',
-  role: 'N/A',
 };
